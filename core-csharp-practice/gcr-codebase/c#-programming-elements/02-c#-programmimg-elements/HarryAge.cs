655a9cbb4234f11bbf776a74b256a81cac4830e2
Ashish-Dubey-1974 <adg1974sop@gmail.com>
2025-12-19 23:10:12 +0530

    [Ashish Dubey] Add C# Practice Questions

--- core-csharp-practice/gcr-codebase/c#-programming-elements/02-c#-programmimg-elements/HarryAge.cs
+++
@@ -1,8 +0,0 @@
-class HarryAge
-{
-    public static void Main(string[] birthYear)
-    {
-        int a = int.Parse(birthYear[0]);
-        Console.WriteLine($"Harry's age in 2024 is {2024-a}");
-    }
-}
