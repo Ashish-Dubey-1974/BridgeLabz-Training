f8b6702b5c22ae48c3f215847650be22e3a148a1
Ashish-Dubey-1974 <adg1974sop@gmail.com>
2025-12-30 20:07:29 +0530

    Stop tracking core-csharp-practice using .gitignore

--- core-csharp-practice/gcr-codebase/c#-arrays/level-1/MultiplicationTable.cs
+++
@@ -1,9 +0,0 @@
-using System;
-class MultiplicationTable{
-    static void Main(){
-        int n=int.Parse(Console.ReadLine());
-        int[] t=new int[10];
-        for(int i=1;i<=10;i++) t[i-1]=n*i;
-        for(int i=1;i<=10;i++) Console.WriteLine(n+" * "+i+" = "+t[i-1]);
-    }
-}
