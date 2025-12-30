b0854603139315cd98510d593b608c41651c77c4
Ashish Dubey <161051825+Ashish-Dubey-1974@users.noreply.github.com>
2025-12-30 22:22:52 +0530

    Delete core-csharp-practice directory

--- core-csharp-practice/gcr-codebase/c#-control-flow/level-1/CountdownFor.cs
+++
@@ -1,9 +0,0 @@
-using System;
-class CountdownFor
-{
-    public static void Main()
-    {
-        int n = int.Parse(Console.ReadLine());
-        for (int i=n; i>= 1; i--)Console.WriteLine(i);
-    }
-}
