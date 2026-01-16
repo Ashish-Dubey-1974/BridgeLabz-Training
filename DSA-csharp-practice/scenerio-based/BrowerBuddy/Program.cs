4ee4e3ff792cca4e324dc05a47348c24c9af54a6
Ashish-Dubey-1974 <adg1974sop@gmail.com>
2026-01-16 15:42:33 +0530

    [Ashish Dubey] Add Scenerio based files

---
+++ DSA-csharp-practice/scenerio-based/BrowerBuddy/Program.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace BrowserBuddy
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            UserMenu menu = new UserMenu();
+            menu.Show();
+        }
+    }
+}
