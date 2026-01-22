904cc33c2241b0ee9cae9fbde3cf57c07408898a
Ashish-Dubey-1974 <adg1974sop@gmail.com>
2026-01-22 18:58:48 +0530

    [Ashish Dubey] Add practice question files asked in review

---
+++ ReviewCG/22Jan/Program.cs
@@ -0,0 +1,70 @@
+﻿
+using System.Text;
+
+class Program
+{
+    public static void Main()
+    {
+        StringBuilderOperation.Perform();
+    }
+}
+
+// what are boxing and unboxing?
+ 
+// Network issue needs to be fixed 
+ 
+// diff b/w var and dynamic keywords
+ 
+// what are extension method
+ 
+// diff b/w abstraction and interface
+ 
+// diff b/w boxing and unboxing
+ 
+// what are partial types
+ 
+// what are sealed classes
+ 
+// what are nullable types
+ 
+// compilation and execution process of csharp
+ 
+// diff b/w is and has operator
+ 
+// as operator
+ 
+// what is inheritance
+ 
+// diff b/w = and ==
+ 
+// what are iplicit typed locked variable
+ 
+// local*
+ 
+// diff types of string class methods
+ 
+// general structure of csharp program
+ 
+// what is namespace
+ 
+// what is default parameter and parametarised parameters
+ 
+// what is abstract classes
+ 
+// what is static classes
+ 
+// object initialiser
+ 
+// what is constructors
+ 
+// what is access modifiers
+ 
+// what is garbage collector
+ 
+// diff bw value types and reference types
+ 
+// what is anonymus types
+ 
+// structures of enum
+ 
+ 
