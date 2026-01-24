6a5d919cb78f9e40298da48c1c0f86056c6909bd
Ashish Dubey <161051825+Ashish-Dubey-1974@users.noreply.github.com>
2026-01-24 19:57:07 +0530

    Time: 8 ms (37.79%), Space: 44 MB (64.5%) - LeetHub

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cs
@@ -0,0 +1,22 @@
+public class Solution {
+    public int LengthOfLongestSubstring(string s) {
+        HashSet<char> map = new HashSet<char>();
+        Queue<char> q = new Queue<char>();
+        int ans = 0;
+        int com=0;
+        for(int i=0;i<s.Length;i++){
+            if(map.Contains(s[i])){
+                ans = ans>com?ans:com;
+                while(q.Peek()!=s[i]){
+                    map.Remove(q.Dequeue());
+                    com--;
+                }q.Dequeue();
+                q.Enqueue(s[i]);
+            }else{
+                map.Add(s[i]);
+                q.Enqueue(s[i]);
+                com++;
+            }
+        }return ans>com?ans:com;
+    }
+}
