e090c32ffb11cbff5374506dcbd19c64768cb824
Ashish Dubey <161051825+Ashish-Dubey-1974@users.noreply.github.com>
2026-01-24 21:37:42 +0530

    Time: 0 ms (100%), Space: 51.3 MB (28.1%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cs
@@ -0,0 +1,8 @@
+public class Solution {
+    public int RemoveDuplicates(int[] nums) {
+        int ans =0;
+        for(int i=1;i<nums.Length;i++){
+            if(nums[i]!=nums[i-1])nums[++ans] = nums[i];
+        }return ans+1;
+    }
+}
