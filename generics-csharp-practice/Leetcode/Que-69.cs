53ad89f39a27bf744cc4955ce10a8c8c280f395b
Ashish-Dubey-1974 <adg1974sop@gmail.com>
2026-02-02 15:16:11 +0530

    [Ashish Dubey] Add Leetcode DSA Practice Problem

---
+++ generics-csharp-practice/Leetcode/Que-69.cs
@@ -0,0 +1,17 @@
+// 69. Sqrt(x)
+// Solved
+// Easy
+
+public class Solution {
+    public int MySqrt(int x) {
+        if(x<2)return x;
+        int l=1,r=x/2;
+        while(l<=r){
+            int mid = l+(r-l)/2;
+            long sq = (long)mid*mid;
+            if(sq==x)return mid;
+            else if(sq<x)l = mid+1;
+            else r = mid-1;
+        }return r;
+    }
+}
