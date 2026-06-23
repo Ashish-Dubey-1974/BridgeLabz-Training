fa2b1e4375d46154c06be740f98fc10dd52ae823
Ashish Dubey <adg1974sop@gmail.com>
2026-06-23 12:21:02 +0530

    Time: 5 ms (93.61%), Space: 54.5 MB (18.41%) - LeetHub

--- 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.cs
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.cs
@@ -1,22 +1,11 @@
 public class Solution {
     public string RemoveStars(string s) {
-        Stack<char> st = new Stack<char>();
+        char[] arr = new char[s.Length];
+        int n=0;
         foreach(char c in s){
-            if(c=='*')st.Pop();
-            else st.Push(c);
-        }StringBuilder sb = new StringBuilder();
-        while(st.Count()!=0)sb.Append(st.Pop());
-        Reverse(sb);
-        return sb.ToString();
-    }void Reverse(StringBuilder sb){
-        int left =0;
-        int right=sb.Length-1;
-        while(left<right){
-            char temp = sb[left];
-            sb[left]=sb[right];
-            sb[right]=temp;
-            right--;
-            left++;
+            if(c=='*')n--;
+            else arr[n++]=c;
         }
+        return new String(arr.AsSpan()[..n]);
     }
 }
